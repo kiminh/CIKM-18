dcee7dcf47f47f092a5eb192b2c9eb1a1bac6cab
Uppinder Chugh <uppinderchugh@gmail.com>
2018-04-24 20:59:29 +0530

    first commit

---
+++ readMe.txt
@@ -0,0 +1,3 @@
+This folder has a network which follow the following schema:
+nodes :  1. Topic, 2. Venue, 3. Author, 4. Feild, 5. Paper
+relations : 1. Topic to Paper, 2. Paper to Author, 3. Venue to Feild, 4. Paper to Venue
---
+++ variables.py
@@ -0,0 +1,22 @@
+# List of author ids
+A = []
+
+# List of paper ids
+P = []
+
+# List of conference ids
+C = []
+
+# Author - Paper Graph
+Gp = None
+
+# Author - Conference Graph
+Gc = None
+
+# Author - Author matrix for A-P-A links
+Ap = None
+
+# Author - Author matrix for A-C-A links
+# Note: These intrinsically contain A-P-A links as well.
+Ac = None
+
